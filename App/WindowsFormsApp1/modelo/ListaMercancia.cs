a2ac46a323eceaa3db818c4af9b3098cac70b904
gfranb <103999749+gfranb@users.noreply.github.com>
2022-10-25 21:11:57 +0200

    Modelo y mercancia lista
    Creados los modelos de lista y mercancia

---
+++ App/WindowsFormsApp1/modelo/ListaMercancia.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.modelo
+{
+    internal class ListaMercancia
+    {
+        private List<Mercancia> listMercancia;
+
+        ListaMercancia(List<Mercancia> listMercancia)
+        {
+            this.listMercancia = listMercancia;
+        }
+
+        ListaMercancia()
+        {
+
+        }
+
+        public void setListaMercancia(List<Mercancia> listMercancia)
+        {
+            this.listMercancia = listMercancia;
+        }
+
+        public List<Mercancia> getListaMercancia()
+        {
+            return listMercancia;
+        }
+
+    }
+}
---
+++ App/WindowsFormsApp1/modelo/Mercancia.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.modelo
+{
+    internal class Mercancia
+    {
+        private string id_mercancia;
+        private float volumenProducto;
+        private string nombre;
+
+        Mercancia(string id_mercancia, float volumenProducto, string nombre)
+        {
+            this.id_mercancia = id_mercancia;
+            this.volumenProducto = volumenProducto;
+            this.nombre = nombre;
+        }
+
+        Mercancia()
+        {
+
+        }
+
+        public void set_id_mercancia(string id)
+        {
+            this.id_mercancia = id;
+        }
+
+        public void set_volumenProducto(float volumen)
+        {
+            this.volumenProducto = volumen;
+        }
+
+        public void set_nombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public string get_nombre()
+        {
+            return this.nombre;
+        }
+
+        public string get_id_mercancia()
+        {
+            return this.id_mercancia;
+        }
+
+        public string get_volumenProducto()
+        {
+            return this.nombre;
+        }
+
+    }
+}
